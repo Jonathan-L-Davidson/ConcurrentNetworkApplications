e24af36fec2fcc186b3f9d15d37eadff0ea839ae
Jonathan Davidson <d016527j@student.staffs.ac.uk>
2020-10-09 17:59:47 +0100

    Task 1 Finsihed

--- CNAWeek2/CNAWeek2/Program.cs
+++ CNAWeek2/CNAWeek2/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Threading;
 
-namespace CNAWeek2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace CNAWeek2 {
+    public class Multithreading {
+        public static void Function() {
+            for (int i = 100; i >= 0; i--) {
+                Console.WriteLine("Thread 1: " + i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Thread thread1 = new Thread(Multithreading.Function);
+            thread1.Start();
+
+            for (int i = 0; i <= 100; i++) {
+                Console.WriteLine("Main: " + i);
+                Thread.Sleep(100);
+            }
         }
     }
 }
