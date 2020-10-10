56919b5dc7f3f33f6505fbc8226e2118dbd30731
Jonathan Davidson <d016527j@student.staffs.ac.uk>
2020-10-10 14:36:36 +0100

    Finished Task 3 & Task 4

---
+++ CNAWeek2/CNAWeek2Task3/main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CNAWeek2Task2 {
+    public class Multithreading {
+        private int id;
+        private int start;
+        private int end;
+
+        public Multithreading(int _identity, int _start = 0, int _end = 10) {
+            id       = _identity;
+            start    = _start;
+            end      = _end;
+        }
+
+        public void Function() {
+            for (int i = start; i <= end; i++) {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+
+            Multithreading mt1 = new Multithreading(0, end, end + 10);
+            Multithreading mt2 = new Multithreading(0, end, end + 10);
+
+            Thread thread1 = new Thread(new ThreadStart(mt1.Function));
+            Thread thread2 = new Thread(new ThreadStart(mt2.Function));
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+
+    class main {
+        static void Main(string[] args) {
+
+            Multithreading mt = new Multithreading(1);
+            Thread thread = new Thread(new ThreadStart(mt.Function));
+            thread.Start();
+        }
+    }
+}
---
+++ CNAWeek2/CNAWeek2Task4/main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CNAWeek2Task4 {
+
+
+
+    class main {
+        static void Main(string[] args) {
+            const int threadAmount = 1000;
+
+            for (int i = 0; i <= threadAmount; i++) {
+                int num = i;
+                new Thread(() => CheckIfPrimeNumber(num)).Start();
+            }
+        }
+        public static void CheckIfPrimeNumber(int number) {
+            double numberRoot = Math.Sqrt(number);
+            int numberRootInt = (int)numberRoot;
+
+            bool isPrime = true;
+
+            if (numberRoot - numberRootInt != 0) {
+                for (int i = 2; i < number; i++) {
+                    if (number % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) Console.WriteLine("Prime Number Found: " + number);
+            }
+        }
+
+    }
+}
